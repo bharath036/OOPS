821a8c840895bc33e154910d1c270445d642da0e
Bharath Kalakoti <bharathrk036@gmail.com>
2025-03-18 19:19:53 +0530

    Add files via upload

--- Day_2_OOPS.py
+++ Day_2_OOPS.py
@@ -413,11 +413,11 @@ A @staticmethod to validate account numbers.
 '''
 
 class BankAccount:
-    def __init__(self):
-        pass 
+    def __init__(self,balance):
+        self.balance = balance 
 
     def deposit(self,):
         pass
 
     def withdraw(self):
-        pass 
+        pass  
